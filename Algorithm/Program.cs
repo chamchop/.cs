970d7bfe5c616113f05ed5ebc59a5a27ed563003
chamchop <ocfmiller@protonmail.com>
2024-06-08 15:43:30 +0100

    algorithms

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,6 +1,6 @@
-﻿using Algorithms.Easy;
+﻿using Algorithm.Easy;
 
-/*var ts = new TwoSum();
+var ts = new TwoSum();
 int[] inputts = { 1, 2, 3, 4, 5 };
 var result = ts.method(inputts, 11);
 Console.WriteLine(result[0]);
@@ -9,12 +9,9 @@ Console.WriteLine(result[1]);
 var ri = new RomanToInteger();
 Console.WriteLine(ri.RomanToInt("IV"));
 Console.WriteLine(ri.RomanToInt("MCMXCIV"));
-Console.WriteLine(ri.solution("IV"));*/
+Console.WriteLine(ri.solution("IV"));
 
-/*var lcp = new LongCommonPrefix();
+var lcp = new LongCommonPrefix();
 string[] inputlcp = { "testing", "testament", "testing", "taste" };
 Console.WriteLine(lcp.LongestCommonPrefix(inputlcp));
-Console.WriteLine(lcp.LongestCommonPrefix(inputlcp));*/
-
-var pally = new Palindrome();
-Console.WriteLine(pally.isPalindrome(121));
+Console.WriteLine(lcp.LongestCommonPrefix(inputlcp));
