42381a3265f8617f419e41f9ba4665b484f6355e
chamchop <ocfmiller@protonmail.com>
2024-06-11 19:09:54 +0100

    rmdir

---
+++ Algorithm/Program.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.Easy;
+using System;
+
+/*var ts = new TwoSum();
+int[] inputts = { 1, 2, 3, 4, 5 };
+var result = ts.method(inputts, 11);
+Console.WriteLine(result[0]);
+Console.WriteLine(result[1]);
+
+var ri = new RomanToInteger();
+Console.WriteLine(ri.RomanToInt("IV"));
+Console.WriteLine(ri.RomanToInt("MCMXCIV"));
+Console.WriteLine(ri.solution("IV"));
+
+var lcp = new LongCommonPrefix();
+string[] inputlcp = { "testing", "testament", "testing", "taste" };
+Console.WriteLine(lcp.LongestCommonPrefix(inputlcp));
+Console.WriteLine(lcp.LongestCommonPrefix(inputlcp));
+
+var pally = new Palindrome();
+Console.WriteLine(pally.isPalindrome(121));*/
+
+var vp = new ValidParentheses();
+string[] vpinput = { "{}" };
+foreach(string test in vpinput)
+{
+    Console.WriteLine(vp.IsValid(test));
+}
